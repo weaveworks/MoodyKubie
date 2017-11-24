56869abc38a5c9cab74bf079d3ed22f3ef240283
Brice Fernandes <brice@weave.works>
2017-11-24 14:39:55 +0000

    Make sure we add the static files

--- web-service/main.go
+++ web-service/main.go
@@ -1,16 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 )
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, %s!", r.URL.Path[1:])
-}
 
 func home(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "./static/")
