584f620b2ec4fc8d1cf81d41edffee9c0b1a78e9
Marc CARRE <carre.marc@gmail.com>
2017-12-05 22:50:59 +0000

    Improve format and use more recent JS syntax.

--- moodykubie-service/app/index.js
+++ moodykubie-service/app/index.js
@@ -1,24 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const emotions = require("./emotions");
 
-
-var express = require('express')
-var bodyParser = require('body-parser');
-
-var emotions = require("./emotions");
-
-
-
-
-var app = express()
+const app = express();
 app.use(bodyParser.raw({
     inflate: true,
     limit: '100kb',
     type: 'application/octet-stream'
 }));
 
-function once(fn, context){
+function once(fn, context) {
     var execd = false;
-    return function(){
-        if (!execd){
+    return function() {
+        if (!execd) {
             execd = true;
             return fn.apply(context || this, arguments);
         }
@@ -26,26 +20,24 @@ function once(fn, context){
 }
 
 app.post('/classify-emotions', function (req, res) {
-
-    console.log("Classifying image")
+    console.log("Classifying image");
     var canvas = null;
     try{
         var tracker = emotions.configuredTracker();
         var classifier = emotions.configuredClassifier();
-        emotions.getEmotions(tracker, classifier, req.body, once(function (reply){
-            console.log("Image Classified after " + reply.iterations + " iterations")
-            res.json(reply)
+        emotions.getEmotions(tracker, classifier, req.body, once(function (reply) {
+            console.log("Image Classified after " + reply.iterations + " iterations");
+            res.json(reply);
         }));
-    }catch (e){
+    } catch (e) {
         console.error(e);
-        res.status(500).json({error: "Failed to process image."})
+        res.status(500).json({error: "Failed to process image."});
     }
-})
+});
 
-console.log("Emotion detection service live on port 8989")
-app.listen(8989)
+console.log("Emotion detection service live on port 8989");
+app.listen(8989);
 
-process.on('uncaughtException', function(err){
-    console.error(err)
-})
-
+process.on('uncaughtException', function(err) {
+    console.error(err);
+});
